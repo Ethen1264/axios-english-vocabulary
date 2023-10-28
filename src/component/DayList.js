3b1bdef046359b0f007cf15ebab01654b7a3a145
Ethen1264 <jxorhks86909763@gmail.com>
2023-10-28 23:45:48 +0900

    setting

---
+++ src/component/DayList.js
@@ -0,0 +1,16 @@
+import dummy from "../db/data.json"
+
+export default function DayList() {
+  console.log(dummy)
+  return<ul>
+    <li className='list_day'>
+      {
+        dummy.days.map(day=>(
+          <li key={day.id}>
+            Day{day.day}
+          </li>
+        ))
+      }
+    </li>
+  </ul>
+}
